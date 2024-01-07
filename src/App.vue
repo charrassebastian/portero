ef2a34272248432f3eb7d1c658c902b8f4bc8227
Sebastian Horacio Charras <sebastiahoraciocharras@gmail.com>
2024-01-06 22:09:28 -0300

    Improved the interface

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,51 @@
 <script setup>
-  import { ref } from 'vue'
-  const message = ref("Se puede pasar")
-  const readMessage = () => {
-    const utterance = new SpeechSynthesisUtterance()
-    utterance.lang = 'es-AR'
-    utterance.text = message.value
-    speechSynthesis.speak(utterance)
-  }
+import { ref } from 'vue'
+const message = ref('Se puede pasar')
+const readMessage = () => {
+  const utterance = new SpeechSynthesisUtterance()
+  utterance.lang = 'es-AR'
+  utterance.text = message.value
+  speechSynthesis.speak(utterance)
+}
 </script>
 
 <template>
   <main>
     <h1>Portero</h1>
-    <label for="msg">Mensaje a leer</label>
-    <input v-model="message" id="msg" placeholder="se puede pasar" />
-    <button @click="readMessage">Leer mensaje</button>
+    <div class="field">
+      <v-text-field
+        color="primary"
+        bg-color="white"
+        id="msg-field"
+        label="Mensaje a leer"
+        v-model="message"
+        placeholder="se puede pasar"
+      />
+    </div>
+    <v-btn color="primary" @click="readMessage">Leer mensaje</v-btn>
   </main>
 </template>
 
 <style scoped>
+.field {
+  width: 100%;
+  padding: 1em;
+}
+
+main {
+  background-image: url('assets/background.png');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  color: white;
+}
 </style>
