fb115aba86c839c5ec00c58be4906f6833519110
Sebastian Charras <sebastianhoraciocharras@gmail.com>
2024-01-07 16:50:41 -0300

    Added licenses

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
 import { ref } from 'vue'
+import Licenses from './components/licenses.vue'
 const message = ref('Se puede pasar')
 const readMessage = () => {
   const utterance = new SpeechSynthesisUtterance()
   utterance.lang = 'es-AR'
   utterance.text = message.value
   speechSynthesis.speak(utterance)
+}
+const shouldDisplayLicenses = ref(false)
+const toggleLicenses = () => {
+  shouldDisplayLicenses.value = !shouldDisplayLicenses.value
+  console.log(shouldDisplayLicenses.value)
 }
 </script>
 
@@ -23,6 +29,8 @@ const readMessage = () => {
       />
     </div>
     <v-btn color="primary" @click="readMessage">Leer mensaje</v-btn>
+    <v-btn variant="plain" class="btn" @click="toggleLicenses">Mostrar licencias</v-btn>
+    <Licenses v-if="shouldDisplayLicenses" />
   </main>
 </template>
 
@@ -39,6 +47,7 @@ main {
   background-size: cover;
   width: 100vw;
   height: 100vh;
+  overflow-y: scroll;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -46,6 +55,11 @@ main {
 }
 
 h1 {
+  color: white;
+}
+
+.btn {
+  margin: 2em;
   color: white;
 }
 </style>
---
+++ src/components/licenses.vue
@@ -0,0 +1,289 @@
+<template>
+    <div>
+        <p>The following npm package may be included in this product: </p><br />
+        <p> - source-map-js@1.0.2 </p><br />
+        <p>This package contains the following license and notice below: </p><br />
+        <p>Copyright (c) 2009-2011, Mozilla Foundation and contributors All rights reserved. </p><br />
+        <p>Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+            following conditions are met: </p><br />
+        <p>* Redistributions of source code must retain the above copyright notice, this list of conditions and the
+            following
+            disclaimer. </p><br />
+        <p>* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+            following
+            disclaimer in the documentation and/or other materials provided with the distribution. </p><br />
+        <p>* Neither the names of the Mozilla Foundation nor the names of project contributors may be used to endorse or
+            promote
+            products derived from this software without specific prior written permission. </p><br />
+        <p>THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+            WARRANTIES,
+            INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+            ARE
+            DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+            INCIDENTAL,
+            SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+            SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+            LIABILITY,
+            WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+            USE
+            OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. </p><br />
+        <p>----------- </p><br />
+        <p>The following npm package may be included in this product: </p><br />
+        <p> - @babel/parser@7.23.6 </p><br />
+        <p>This package contains the following license and notice below: </p><br />
+        <p>Copyright (C) 2012-2014 by various contributors (see AUTHORS) </p><br />
+        <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+            documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+            the
+            rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+            permit
+            persons to whom the Software is furnished to do so, subject to the following conditions: </p><br />
+        <p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+            the
+            Software. </p><br />
+        <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+            THE
+            WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+            AUTHORS OR
+            COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+            OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+        </p>
+        <br />
+        <p>----------- </p><br />
+        <p>The following npm package may be included in this product: </p><br />
+        <p> - estree-walker@2.0.2 </p><br />
+        <p>This package contains the following license and notice below: </p><br />
+        <p>Copyright (c) 2015-20 [these people](https://github.com/Rich-Harris/estree-walker/graphs/contributors) </p><br />
+        <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+            documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+            the
+            rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+            permit
+            persons to whom the Software is furnished to do so, subject to the following conditions: </p><br />
+        <p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+            the
+            Software. </p><br />
+        <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+            THE
+            WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+            AUTHORS OR
+            COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+            OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+        </p>
+        <br />
+        <p>----------- </p><br />
+        <p>The following npm package may be included in this product: </p><br />
+        <p> - csstype@3.1.3 </p><br />
+        <p>This package contains the following license and notice below: </p><br />
+        <p>Copyright (c) 2017-2018 Fredrik Nicol </p><br />
+        <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+            documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+            the
+            rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+            permit
+            persons to whom the Software is furnished to do so, subject to the following conditions: </p><br />
+        <p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+            the
+            Software. </p><br />
+        <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+            THE
+            WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+            AUTHORS OR
+            COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+            OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+        </p>
+        <br />
+        <p>----------- </p><br />
+        <p>The following npm package may be included in this product: </p><br />
+        <p> - entities@4.5.0 </p><br />
+        <p>This package contains the following license and notice below: </p><br />
+        <p>Copyright (c) Felix Böhm All rights reserved. </p><br />
+        <p>Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+            following conditions are met: </p><br />
+        <p>Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+            disclaimer. </p><br />
+        <p>Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+            following
+            disclaimer in the documentation and/or other materials provided with the distribution. </p><br />
+        <p>THIS IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+            INCLUDING,
+            BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+            DISCLAIMED. IN
+            NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+            EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+            LOSS
+            OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+            CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+            THIS,
+            EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. </p><br />
+        <p>----------- </p><br />
+        <p>The following npm package may be included in this product: </p><br />
+        <p> - magic-string@0.30.5 </p><br />
+        <p>This package contains the following license and notice below: </p><br />
+        <p>Copyright 2018 Rich Harris </p><br />
+        <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+            documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+            the
+            rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+            permit
+            persons to whom the Software is furnished to do so, subject to the following conditions: </p><br />
+        <p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+            the
+            Software. </p><br />
+        <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+            THE
+            WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+            AUTHORS OR
+            COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+            OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+        </p>
+        <br />
+        <p>----------- </p><br />
+        <p>The following npm package may be included in this product: </p><br />
+        <p> - picocolors@1.0.0 </p><br />
+        <p>This package contains the following license and notice below: </p><br />
+        <p>ISC License </p><br />
+        <p>Copyright (c) 2021 Alexey Raspopov, Kostiantyn Denysov, Anton Verinov </p><br />
+        <p>Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby
+            granted, provided that the above copyright notice and this permission notice appear in all copies. </p><br />
+        <p>THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING
+            ALL
+            IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL,
+            DIRECT,
+            INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+            WHETHER IN
+            AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+            PERFORMANCE OF THIS SOFTWARE. </p><br />
+        <p>----------- </p><br />
+        <p>The following npm package may be included in this product: </p><br />
+        <p> - @jridgewell/sourcemap-codec@1.4.15 </p><br />
+        <p>This package contains the following license and notice below: </p><br />
+        <p>The MIT License </p><br />
+        <p>Copyright (c) 2015 Rich Harris </p><br />
+        <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+            documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+            the
+            rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+            permit
+            persons to whom the Software is furnished to do so, subject to the following conditions: </p><br />
+        <p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+            the
+            Software. </p><br />
+        <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+            THE
+            WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+            AUTHORS OR
+            COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+            OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+        </p>
+        <br />
+        <p>----------- </p><br />
+        <p>The following npm package may be included in this product: </p><br />
+        <p> - vuetify@3.4.9 </p><br />
+        <p>This package contains the following license and notice below: </p><br />
+        <p>The MIT License (MIT) </p><br />
+        <p>Copyright (c) 2016-2023 John Jeremy Leider </p><br />
+        <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+            documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+            the
+            rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+            permit
+            persons to whom the Software is furnished to do so, subject to the following conditions: </p><br />
+        <p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+            the
+            Software. </p><br />
+        <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+            THE
+            WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+            AUTHORS OR
+            COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+            OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+        </p>
+        <br />
+        <p>----------- </p><br />
+        <p>The following npm packages may be included in this product: </p><br />
+        <p> - @vue/compiler-core@3.4.5 - @vue/compiler-dom@3.4.5 - @vue/compiler-sfc@3.4.5 - @vue/compiler-ssr@3.4.5 -
+            @vue/reactivity@3.4.5 - @vue/runtime-core@3.4.5 - @vue/runtime-dom@3.4.5 - @vue/server-renderer@3.4.5 -
+            @vue/shared@3.4.5 - vue@3.4.5 </p><br />
+        <p>These packages each contain the following license and notice below: </p><br />
+        <p>The MIT License (MIT) </p><br />
+        <p>Copyright (c) 2018-present, Yuxi (Evan) You </p><br />
+    <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+        documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+        the
+        rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+        permit
+        persons to whom the Software is furnished to do so, subject to the following conditions: </p><br />
+    <p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+        the
+        Software. </p><br />
+    <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+        THE
+        WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR
+        COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+        OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    </p>
+    <br />
+    <p>----------- </p><br />
+    <p>The following npm package may be included in this product: </p><br />
+    <p> - postcss@8.4.33 </p><br />
+    <p>This package contains the following license and notice below: </p><br />
+    <p>The MIT License (MIT) </p><br />
+    <p>Copyright 2013 Andrey Sitnik &lt;andrey@sitnik.ru&gt;
+    </p><br />
+    <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+        documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+        the
+        rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+        permit
+        persons to whom the Software is furnished to do so, subject to the following conditions: </p><br />
+    <p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+        the
+        Software. </p><br />
+    <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+        THE
+        WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR
+        COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+        OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    </p>
+    <br />
+    <p>----------- </p><br />
+    <p>The following npm package may be included in this product: </p><br />
+    <p> - nanoid@3.3.7 </p><br />
+    <p>This package contains the following license and notice below: </p><br />
+    <p>The MIT License (MIT) </p><br />
+    <p>Copyright 2017 Andrey Sitnik &lt;andrey@sitnik.ru&gt;
+    </p><br />
+    <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+        documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+        the
+        rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+        permit
+        persons to whom the Software is furnished to do so, subject to the following conditions: </p><br />
+    <p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+        the
+        Software. </p><br />
+    <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+        THE
+        WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR
+        COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+        OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    </p>
+    <br />
+    <p>-----------</p>
+</div>
+</template>
+<style scoped>
+div {
+    width: 100%;
+    padding: 2em;
+    overflow-x: hidden;
+}
+
+p {
+    color: white;
+}
+</style>
