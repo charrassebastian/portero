caffe625903a0e9ee236e38c94c2bf4d9a07ae8e
Sebastian Charras <sebastianhoraciocharras@gmail.com>
2024-01-07 16:12:16 -0300

    now the pwa can be installed

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,28 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     vue(), 
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "logo.svg"],
+      manifest: {
+        name: "Portero",
+        short_name: "Portero",
+        description: "Portero",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      }
+    })
   ],
   resolve: {
     alias: {
