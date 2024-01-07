fa872755f042bc5326918565053ed54f2ad31642
Sebastian Horacio Charras <sebastiahoraciocharras@gmail.com>
2024-01-06 21:35:34 -0300

    It works

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,22 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import { ref } from 'vue'
+  const message = ref("Se puede pasar")
+  const readMessage = () => {
+    const utterance = new SpeechSynthesisUtterance()
+    utterance.lang = 'es-AR'
+    utterance.text = message.value
+    speechSynthesis.speak(utterance)
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <h1>Portero</h1>
+    <label for="msg">Mensaje a leer</label>
+    <input v-model="message" id="msg" placeholder="se puede pasar" />
+    <button @click="readMessage">Leer mensaje</button>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
